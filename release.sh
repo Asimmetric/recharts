8f090907d682bcd8313728880587316022b76e9e
Alex L <aleib.start@gmail.com>
2017-08-04 13:13:47 +0200

    Version 1.0.34

--- release.sh
+++ release.sh
@@ -4,4 +4,5 @@ RELEASE_ID=$(curl --data "{\"tag_name\": \"v1.0.$COMMIT_COUNT\", \"target_commit
 echo "Creating distribution tarball"
 tar -cvzf release.tar.gz README.md package.json LICENSE umd/ lib/ es6/
 echo "Uploading tarball artifact to release"
+echo $RELEASE_ID
 curl -XPOST -H "Authorization:token $GITHUB_AUTH_TOKEN" -H "Content-Type:application/octet-stream" --data-binary @release.tar.gz https://uploads.github.com/repos/Asimmetric/recharts/releases/$RELEASE_ID/assets?name=release.tar.gz
